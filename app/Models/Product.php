ac6717bbbff557ecff3aeb8cdf1b24a64ca53a84
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-04 20:56:44 +0100

    Created models and migrations

---
+++ app/Models/Product.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    public function User()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function Brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function Category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function Supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+
+    public function Card()
+    {
+        return $this->belongsTo(Card::class);
+    }
+}
