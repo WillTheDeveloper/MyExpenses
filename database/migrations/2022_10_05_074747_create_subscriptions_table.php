88f8871209dd795b66bf4d32c438325a62991c8f
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-05 21:24:31 +0100

    Fixed migration issue

--- database/migrations/2022_10_05_074747_create_subscriptions_table.php
+++ database/migrations/2022_10_05_074747_create_subscriptions_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->integer('user_id')->nullable(false);
             $table->integer('card_id')->nullable(false);
-            $table->date('started_on')->default(now()->toDate());
+            $table->date('started_on')->default(now());
             $table->decimal('cost', 5, 2);
             $table->enum('type', ['monthly', 'yearly'])->default('monthly');
             $table->timestamps();
