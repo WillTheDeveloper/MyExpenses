eb1bf8cfffc35fd83f60f4c8415db6aecd308d02
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-05 08:58:09 +0100

    Finished subscription table and made pk a uuid

--- database/migrations/2022_10_05_074747_create_subscriptions_table.php
+++ database/migrations/2022_10_05_074747_create_subscriptions_table.php
@@ -14,7 +14,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('subscriptions', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->integer('user_id')->nullable(false);
+            $table->integer('card_id')->nullable(false);
+            $table->date('started_on')->default(now()->toDate());
+            $table->decimal('cost', 5, 2);
+            $table->enum('type', ['monthly', 'yearly'])->default('monthly');
             $table->timestamps();
         });
     }
