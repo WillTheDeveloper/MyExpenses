e50437e91d98fb162058119f04cb5e4a0d9dd6d6
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-06 11:26:36 +0100

    Added some stats to the dashboard

---
+++ app/Http/Controllers/Dashboard.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class Dashboard extends Controller
+{
+    public function view()
+    {
+        $t = Product::query()->whereMonth('created_at', \Illuminate\Support\Carbon::now()->month)->sum('cost');
+        $l = Product::query()->whereMonth('created_at', \Illuminate\Support\Carbon::now()->subMonth())->sum('cost');
+
+        if ($t == 0 or $l == 0){
+            $d = 0;
+        }
+
+        return view('dashboard', [
+            'thismonth' => $t,
+            'lastmonth' => $l,
+            'difference' => $d
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,5 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', [\App\Http\Controllers\Dashboard::class, 'view'])->name('dashboard');
 });
