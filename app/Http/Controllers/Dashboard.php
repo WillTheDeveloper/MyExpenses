4feb7846ffac2ebe6771fa9e9c936fd59fe45e9c
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-06 11:48:37 +0100

    Tidied up eloquent query

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -10,8 +10,14 @@ class Dashboard extends Controller
 {
     public function view()
     {
-        $t = Product::query()->whereMonth('created_at', \Illuminate\Support\Carbon::now()->month)->sum('cost');
-        $l = Product::query()->whereMonth('created_at', \Illuminate\Support\Carbon::now()->subMonth())->sum('cost');
+        $t = Product::query()
+            ->whereMonth('created_at', \Illuminate\Support\Carbon::now()->month)
+            ->whereYear('created_at', \Illuminate\Support\Carbon::now()->year)
+            ->sum('cost');
+        $l = Product::query()
+            ->whereMonth('created_at', \Illuminate\Support\Carbon::now()->subMonth())
+            ->whereYear('created_at', \Illuminate\Support\Carbon::now()->year)
+            ->sum('cost');
 
         if ($t == 0 or $l == 0){
             $d = 0;
