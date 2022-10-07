f534c3a600594e7874d53d59d0d19a20360d5508
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-10-07 09:54:05 +0100

    Fixed earnings table

--- app/Http/Controllers/Earning.php
+++ app/Http/Controllers/Earning.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class Earning extends Controller
 {
     public function view()
     {
-        return view('earnings');
+        return view('earnings', [
+            'list' => \App\Models\Earning::query()
+                ->whereYear('created_at', Carbon::now()->year)
+                ->where('user_id', auth()->id())
+                ->paginate(12)
+        ]);
     }
 }
--- resources/views/earnings.blade.php
+++ resources/views/earnings.blade.php
@@ -60,15 +60,17 @@
                                         </thead>
                                         <tbody class="divide-y divide-gray-200 bg-white">
 
-                                        <tr>
-                                            <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">Lindsay Walton</td>
-                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Front-end Developer</td>
-                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">lindsay.walton@example.com</td>
-                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Member</td>
-                                            <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
-                                            </td>
-                                        </tr>
+                                        @foreach($list as $l)
+                                            <tr>
+                                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{{$l->from}}</td>
+                                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{$l->Card->name}}</td>
+                                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{$l->paid->diffForHumans()}}</td>
+                                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">£{{$l->amount}}</td>
+                                                <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, {{$l->from}}</span></a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
 
                                         <!-- More people... -->
                                         </tbody>
