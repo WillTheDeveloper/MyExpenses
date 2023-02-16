90718d313721b693e33be9239349cef2041c5b62
WillTheDeveloper <willthedeveloper13@gmail.com>
2023-02-16 18:02:34 +0000

    Creating endpoints to add entries to salary

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\Brand;
+use App\Http\Controllers\Card;
+use App\Http\Controllers\Category;
+use App\Http\Controllers\Dashboard;
+use App\Http\Controllers\Earning;
+use App\Http\Controllers\Product;
+use App\Http\Controllers\Subscription;
+use App\Http\Controllers\Supplier;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,21 +16,22 @@ Route::get('/', function () {
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
 
-    Route::get('/dashboard', [\App\Http\Controllers\Dashboard::class, 'view'])->name('dashboard');
+    Route::get('/dashboard', [Dashboard::class, 'view'])->name('dashboard');
 
-    Route::get('/products', [\App\Http\Controllers\Product::class, 'view'])->name('products');
-    Route::get('/products/new', [\App\Http\Controllers\Product::class, 'new'])->name('product.new');
-    Route::post('/products/create', [\App\Http\Controllers\Product::class, 'create'])->name('product.create');
+    Route::get('/products', [Product::class, 'view'])->name('products');
+    Route::get('/products/new', [Product::class, 'new'])->name('product.new');
+    Route::post('/products/create', [Product::class, 'create'])->name('product.create');
 
-    Route::get('/earnings', [\App\Http\Controllers\Earning::class, 'view'])->name('earnings');
+    Route::get('/earnings', [Earning::class, 'view'])->name('earnings');
+    Route::get('/earnings/add', [Earning::class, 'add'])->name('earning.add');
 
-    Route::get('/cards', [\App\Http\Controllers\Card::class, 'view'])->name('cards');
+    Route::get('/cards', [Card::class, 'view'])->name('cards');
 
-    Route::get('/subscriptions', [\App\Http\Controllers\Subscription::class, 'view'])->name('subscriptions');
+    Route::get('/subscriptions', [Subscription::class, 'view'])->name('subscriptions');
 
-    Route::get('/brands', [\App\Http\Controllers\Brand::class, 'view'])->name('brands');
+    Route::get('/brands', [Brand::class, 'view'])->name('brands');
 
-    Route::get('/categories', [\App\Http\Controllers\Category::class, 'view'])->name('categories');
+    Route::get('/categories', [Category::class, 'view'])->name('categories');
 
-    Route::get('/supplier', [\App\Http\Controllers\Supplier::class, 'view'])->name('supplier');
+    Route::get('/supplier', [Supplier::class, 'view'])->name('supplier');
 });
